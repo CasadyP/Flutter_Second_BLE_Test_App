b5d0a8aa1c299e5496bd2e36dfada769e1a4258c
Casady Palmer <46377974+CasadyP@users.noreply.github.com>
2023-07-25 16:00:31 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(home: HomePage()),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+      persistentFooterButtons: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey, // background
+            foregroundColor: Colors.white, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.search),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey, // background
+            foregroundColor: Colors.white, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.bluetooth),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey, // background
+            foregroundColor: Colors.white, // foreground
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.celebration_rounded),
+        ),
+      ],
+    );
+  }
+}
